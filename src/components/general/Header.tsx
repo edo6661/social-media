464a9649c0751f8be075ddc11e536cce7606a995
Edo <1122140027@global.ac.id>
2024-02-12 21:29:44 +0700

    fix types, why the hell ts is so sctrict in vite

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
   const authPath = location.pathname == "/auth";
   const conditionalLink = authPath ? "/" : "/auth";
+
   return (
     <header>
       <Link to={conditionalLink}>{authPath ? "Home" : "Auth"}</Link>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import Providers from "./providers/Providers.tsx";
 import { RouterProvider } from "react-router-dom";
